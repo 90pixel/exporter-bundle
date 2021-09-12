7da789a5df02feba215c9e4480137d2daad6cac6
Muhep Atasoy <muhepatasoy06@gmail.com>
2021-09-12 20:00:33 +0300

    Add mission func to interface

--- Interfaces/ExporterInterface.php
+++ Interfaces/ExporterInterface.php
@@ -8,5 +8,7 @@ interface ExporterInterface
 {
     public function builder(QueryBuilder $queryBuilder, string $alias);
 
+    public function row(array $data): array;
+
     public function filters(): array;
 }
